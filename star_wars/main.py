05a99aa4126cdd02116b8885e46b1c1a87419d52
Paula Alves <paulafernandaalves@icloud.com>
2019-10-28 07:38:48 -0200

    Update exercise starwars
    Include class character

--- star_wars/main.py
+++ star_wars/main.py
@@ -1,8 +1,18 @@
 import requests
 
-class Character():
-    def __init__(self):
-        name = self
+class Character:
+
+    def __init__(self, name=None, movies=''):
+        self.name = name        
+        self.movies = movies
+
+    def personagem(self):
+        self.name = None
+        self.movies = ''
+        return self.name, self.movies
+
+    def __repr__(self):
+        return str(self.__dict__)
 
 
 
@@ -25,9 +35,11 @@ class Character():
         ...
     ValueError: this is not the character you are looking for
     """
-    pass
+
+    
 
 def features_together(a, b):
+    pass
     """
     Returns a list of movie where both character a and character b are present
 
@@ -40,25 +52,25 @@ def features_together(a, b):
     ## chamar as duas listas e comparar personagens e filmes
 
 
-    print(a[0])
-    print(b[0])
-    together = []
+    # print(a[0])
+    # print(b[0])
+    # together = []
     
-    together = [x for x in b[0] if x in b[1]]
-    print(a[0] ,' + ', a[1],' = ', together)
+    # together = [x for x in b[0] if x in b[1]]
+    # print(a[0] ,' + ', a[1],' = ', together)
 
-    # loop para o primeiro persogem será:    a[0] compara com [a + 1]
-    #                                        a[0] compara com [a + 1 + 1]
-    #                                        a[0] compara com [a + 1 + 1 + 1]....
-    # depois: a[2] com
+    # # loop para o primeiro persogem será:    a[0] compara com [a + 1]
+    # #                                        a[0] compara com [a + 1 + 1]
+    # #                                        a[0] compara com [a + 1 + 1 + 1]....
+    # # depois: a[2] com
 
-    # COMPARAR FILMES E NAO PERSONAGENS:
-    # compara 
-    lista_de_filmes = b[0] + b [1]
+    # # COMPARAR FILMES E NAO PERSONAGENS:
+    # # compara 
+    # lista_de_filmes = b[0] + b [1]
 
-    print(lista_de_filmes)
+    # print(lista_de_filmes)
 
-    # buscar os filmes pela URL
+    # # buscar os filmes pela URL
 
 
 
@@ -67,10 +79,10 @@ def features_together(a, b):
 
 # busco a completa de infos dentro da url, esta em um dicionario
 planeta = requests.get("https://swapi.co/api/people/?format=json").json()
-# o que eu preciso esta na chave 'results', entao busco ela
+# # o que eu preciso esta na chave 'results', entao busco ela
 m = planeta['results']
 
-#agora, dentro de m, tenho uma lista com varias infos de cade personagem (chaves), sendo que preciso de 'name' e film'
+# #agora, dentro de m, tenho uma lista com varias infos de cade personagem (chaves), sendo que preciso de 'name' e film'
 
 i = 0
 a = []
@@ -81,17 +93,20 @@ while i < len(m):
     b.append(nome['films'])
     i +=1
 
-i = 0
-while i < len(a):
-    print(a[i])
-    print(b[i])
-    i += 1
-
-features_together(a, b)
-    
 # agora tenho duas listas:
 # 1 - com os nomes dos personagens
 # 2 - com lista de lista de filmes de cada um. 
+
+# print(a)
+# print(b)
+
+personagem01 = Character('Luke Skywalker', 'https://swapi.co/api/films/2/')
+personagem02 = Character('C-3PO', 'https://swapi.co/api/films/2/')
+
+print(personagem01)
+print(personagem02)
+
+
 
 if __name__ == "__main__":
     import doctest
